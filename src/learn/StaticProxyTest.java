51c6b47a69ccb2b0e1002f67a1ccff925ce57d15
Mr.MC <1528076507@qq.com>
2023-06-07 22:48:52 +0800

    静态代理的部分理解

--- src/learn/StaticProxyTest.java
+++ src/learn/StaticProxyTest.java
@@ -6,12 +6,21 @@ import org.junit.Test;
 import proxy.UserProxy;
 
 /**
+ * 什么是静态代理？
+ *  静态代理实际上是在不改变被代理对象功能（代码）的前提下，使用代理对象对其进行功能上的拓展。
+ *
+ * 静态代理的特点
+ *  被代理对象和代理对象实现相同的接口
+ *
  * @author Mr.MC
  */
 public class StaticProxyTest {
 
     @Test
     public void test01() {
+        // 被代理类作为代理类的一个成员变量，
+        // 在代理类实现的接口方法中调用该被代理类同样实现的接口方法并增加其他的业务逻辑以对被代理类的方法进行增强
+        // 最后只需要调用代理类的对应方法即可
         UserDao userDao = new UserDaoImpl();
         UserProxy proxy = new UserProxy(userDao);
         proxy.login();
