152309ab87df851f04427674b4f264f64efcba0a
Mr.MC <1528076507@qq.com>
2024-06-05 19:25:27 +0800

    log4j2的相关配置及配置文件加载

--- src/base/ServiceSupport.java
+++ src/base/ServiceSupport.java
@@ -1,5 +1,7 @@
 package base;
 
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.xml.DOMConfigurator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -9,6 +11,13 @@ import org.apache.logging.log4j.Logger;
 public abstract class ServiceSupport {
 
     protected final Logger logger = LogManager.getLogger(this.getClass());
+
+    public static void initLog4j2() {
+        // 加载xml配置文件文件
+        // DOMConfigurator.configure("/log4j2.xml");
+        // 加载properties配置文件
+        PropertyConfigurator.configure("/log4j2.properties");
+    }
 
     public ServiceSupport() {}
 }
